11d7a2e55262ab53c4620878c2ff85bde0562e97
Tesalriah <proella8@gmail.com>
2024-03-02 05:58:46 +0900

    edit terms

--- 프론트/js/terms.js
+++ 프론트/js/terms.js
@@ -1,23 +1,41 @@
-const checkboxes = document.getElementsByName('agree');
+const okBtn = document.querySelector('.ok_btn');
+const cancelBtn = document.querySelector('.cancel_btn');;
+const allBtn = document.querySelector('#agree_all');
 
-function selectAll(selectAll){
-    checkboxes.forEach((checkbox) =>{
-        checkbox.checked = selectAll.checked;
+okBtn.addEventListener("click",function(){
+    if(allBtn.checked == false)
+        alert('J&H 이용약관과 개인정보 수집 및 이용에 대한 안내 모두 동의해주세요.');
+    if(allBtn.checked == true)
+        location.href='signUp.html';
+})
+
+cancelBtn.addEventListener('click', function (){
+    location.href = 'main.html';
+})
+
+allBtn.addEventListener("click", function(){
+    const checkBox = document.querySelectorAll('.check_box');
+    if(checkBox[0].checked == true){
+        for(let i=0; i<checkBox.length; i++){
+            checkBox[i].checked = true;
+        }
+    }else if(checkBox[0].checked == false){
+        for(let i=0; i<checkBox.length; i++){
+            checkBox[i].checked = false;
+        }
+    }
+})
+
+var checkAll = document.querySelectorAll('.check_box');
+
+for(let i=1; i<checkAll.length; i++){
+    checkAll[i].addEventListener("click", function(){
+        console.log(i);
+        if(checkAll[1].checked == true && checkAll[2].checked == true && checkAll[3].checked == true){
+        checkAll[0].checked = true;
+        }
+        if(checkAll[i].checked == false){
+            checkAll[0].checked = checkAll[i].checked;
+        }
     })
-}
-
-function checkAll(checkAll){
-    if(checkboxes[1].checked == true && checkboxes[2].checked == true && checkboxes[3].checked == true){
-        document.querySelector('.check_box').checked = true;
-    }
-    if(checkAll.checked == false){
-        document.querySelector('.check_box').checked = checkAll.checked;
-    }
-}
-
-function checkboxCheck(){
-if(checkboxes[0].checked == false)
-    alert('J&H 이용약관과 개인정보 수집 및 이용에 대한 안내 모두 동의해주세요.');
-if(checkboxes[0].checked == true)
-    location.href='signUp.html';
 }
